6853a8c15500b0c1d3506e18fb151e3e2839c347
Sebastian Kaplun <sebikaplun@gmail.com>
2018-11-14 19:09:42 +0200

    add project skeleton

---
+++ Firebase-Auth-iOS-Facebook/Firebase-Auth-iOS-Facebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-Auth-iOS-Facebook
+//
+//  Created by Sebastian Kaplun on 14/11/2018.
+//  Copyright © 2018 Sebai Kaplun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
